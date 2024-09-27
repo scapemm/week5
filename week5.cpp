66234c48194242ab901718a3de58261cbcd41a24
scapemm <a.shukurullayev@newuu.uz>
2024-09-27 12:11:24 +0500

    Update week5.cpp

--- week5.cpp
+++ week5.cpp
@@ -96,17 +96,126 @@ void problem10(){
     cout << "Average is:"<< sum/(pos+neg)<< "\n";
 
 }
+void problem11(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            cout << '*';
+        }
+        cout << "\n";
+    }
+}
+void problem12(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<=i; ++j){
+            cout << '*';
+        }
+        cout << "\n";
+    }
+}
+void problem13(){
+    int n;
+    cin >> n;
+    if(n <= 0) cout << "The depth is 0\n";
+    else{
+        for(int i=1; i<=n; ++i){
+            for(int j=1; j<=i; ++j){
+                cout << i;
+            }
+            cout << "\n";
+        }
+    }
+}
+void problem14(){
+    long double sum = 0;
+    double n;
+    cin >> n;
+    if(n <= 0) cout << "Not possible\n";
+    for(double i=1; i<=n; ++i){
+        cout << ' ' << 1 << '/' << i << ' ';
+        if(i != n) cout << '+';
+        sum += 1/i;
+    }
+    cout << "\n";
+    cout << "The Sum is : " << sum  << "\n";
+}
+void problem15(){
+    long double cost = 1000*12;
+    cout << "For year number " << 1 << " rent is " << 1000 << "\n";
+    cout << "The total for year 1 is " << cost << "\n";
+    for(int i=2; i<=5; ++i){
+        cost *= 1.03;
+        cout << "For year number " << 1 << " rent is " << cost / 12 << "\n";
+        cout << "The total for year " << i << " is " << cost << "\n";
+    }
+    cout << "For all 5 years total rent is " << cost << "\n";
+}
+void problem16(){
+    int n;
+    cin >> n;
+    int sum = 0;
+    while(n != 0){
+        sum += n % 10;
+        n /= 10;
+    }
+    cout << "The sum is digit is " << sum << "\n";
+}
+void problem17(){
+    int n;
+    cin >> n;
+    int sum = 0;
+    int cnt = 0;
+    while(n != 0){
+        if(n%10==1){
+            sum += pow(2,cnt);
+        }
+        cnt++;
+        n /= 10;
+    }
+    cout << "The decimal number is " << sum << "\n";
+}
+void problem18(){
+    long long n;
+    cin >> n;
+    int cnt = 2;
+    while(n != 1){
+        if(n % cnt == 0){
+            cout << cnt << ' ';
+            n /= cnt;
+            cnt = 2;
+        }
+        else{
+            cnt++;
+        }
+    }
+}
+void problem19(){
+    long double sum = 1;
+    long long n;
+    cin >> n;
+    for(double i=3; i<=)
+}
 int main() {
     // problem1();
     //problem2();
     //problem3();
-   // problem4();
-  // problem5();
- // problem6();
+    // problem4();
+    // problem5();
+    // problem6();
 //  problem7();
     //problem8();
-   // problem9();
-  // problem10();
-
+    // problem9();
+    // problem10();
+   // problem11();
+   //problem12();
+   //problem13();
+  // problem14();
+ // problem15();
+ //problem16();
+   //problem17();
+   //problem18();
     return 0;
 }
